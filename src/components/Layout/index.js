8abe44bf4ee1dfec557dda29b02feb62a616ab38
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-08-22 20:49:24 +0100

    sidebar done

---
+++ src/components/Layout/index.js
@@ -0,0 +1,18 @@
+import "./index.scss"
+import Sidebar from "../Sidebar"
+import { Outlet } from "react-router-dom"
+
+const Layout = () => {
+  return (
+    <div className="App">
+      <Sidebar />
+      <div className="Page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+      </div>
+    </div>
+  )
+}
+
+export default Layout
