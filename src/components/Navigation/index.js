43ffb71b4c33ae5d46bf8ae3ae12735f728f6b3c
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-08-25 13:17:51 +0100

    active nav links done

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHome,
@@ -14,34 +14,59 @@ const Navigation = () => (
     <nav className="nav-bar">
       <ul>
         <li>
-          <Link to="/" className="home-icon">
-            <FontAwesomeIcon icon={faHome} color="#000" />
+          <NavLink
+            to="/"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "home-icon"
+            }
+          >
+            <FontAwesomeIcon icon={faHome} />
             &nbsp;
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/About" className="about-icon">
-            <FontAwesomeIcon icon={faUser} color="#fff" />
+          <NavLink
+            to="/About"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "about-icon"
+            }
+          >
+            <FontAwesomeIcon icon={faUser} />
             &nbsp;
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/Project" className="project-icon">
-            <FontAwesomeIcon icon={faBook} color="#fff" />
+          <NavLink
+            to="/Project"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "project-icon"
+            }
+          >
+            <FontAwesomeIcon icon={faBook} />
             &nbsp;
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/Resume" className="resume-icon">
-            <FontAwesomeIcon icon={faNewspaper} color="#fff" />
+          <NavLink
+            to="/Resume"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "resume-icon"
+            }
+          >
+            <FontAwesomeIcon icon={faNewspaper} />
             &nbsp;
-          </Link>
+          </NavLink>
         </li>
         <li>
-          <Link to="/Contact" className="contact-icon">
-            <FontAwesomeIcon icon={faEnvelope} color="#fff" />
+          <NavLink
+            to="/Contact"
+            className={({ isActive }) =>
+              isActive ? "link-active" : "contact-icon"
+            }
+          >
+            <FontAwesomeIcon icon={faEnvelope} />
             &nbsp;
-          </Link>
+          </NavLink>
         </li>
       </ul>
     </nav>
