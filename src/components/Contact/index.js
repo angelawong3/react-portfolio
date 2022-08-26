c1bb2b1be0a27bd6a61cdc1e24ee9f0a4090c003
angelawong3 <100792114+angelawong3@users.noreply.github.com>
2022-08-26 22:54:17 +0100

    contact form css done

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -5,6 +5,32 @@ export default class Contact extends Component {
     return (
       <section id="section">
         <div id="title">CONTACT ME</div>
+        {/* TODO: notification for required field */}
+        <form>
+          <label className="label">
+            Name:&nbsp;
+            <input className="input-name" type="text" name="name" />
+          </label>
+          {/* TODO: validate email address */}
+          <label className="label">
+            Email:&nbsp;
+            <input
+              className="input-email"
+              type="text"
+              name="email"
+              placeholder="Please enter a valid email address"
+            />
+          </label>
+          <label className="label">
+            Message:&nbsp;
+            <textarea
+              className="input-message"
+              type="text"
+              name="message"
+            ></textarea>
+          </label>
+          <input className="btn-form" type="submit" value="Submit" />
+        </form>
         {/* 
         contact form with fields for a name, an email address, and a message
         WHEN I move my cursor out of one of the form fields without entering text
